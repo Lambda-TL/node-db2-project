a3961b4e8201e7f220c9b680e1089ae261123740
Jessica <duellje@oregonstate.edu>
2021-02-28 20:24:10 -0500

    created cars table migration

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,11 @@
-// DO YOUR MAGIC
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", (tbl) => {
+    tbl.increments("id");
+    tbl.string("vin").notNullable().unique();
+    tbl.string("make").notNullable();
+    tbl.string("model").notNullable();
+    tbl.integer("mileage").notNullable();
+    tbl.string("title");
+    tbl.string("transmission");
+  });
+};
