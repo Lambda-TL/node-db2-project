00723b1e0443eac417b9381d1e6342a65bf79500
Jessica <duellje@oregonstate.edu>
2021-02-28 20:47:13 -0500

    completed middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,60 @@
+const carsModel = require("./cars-model");
+const vinValidator = require("vin-validator");
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const checkCar = carsModel.getById(req.params.id);
+  if (checkCar.length > 0) {
+    next();
+  } else {
+    res
+      .status(404)
+      .json({ message: `car with id ${req.params.id} is not found` });
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  if (req.body) {
+    const { vin, make, model, milage } = req.body;
+    if (!vin) {
+      res.status(400).json({ message: "vin is missing" });
+    } else if (!make) {
+      res.status(400).json({ message: "make is missing" });
+    } else if (!model) {
+      res.status(400).json({ message: "model is missing" });
+    } else if (!milage) {
+      res.status(400).json({ message: "mileage is missing" });
+    } else {
+      next();
+    }
+  } else {
+    res.status(400).json({ message: "vin is missing" });
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  if (vinValidator(req.body.vin)) {
+    next();
+  } else {
+    res.status(400).json({ message: `vin ${req.body.vin} is invalid` });
+  }
+};
 
 const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const carsDb = carsModel.getAll();
+  const vin = req.body.vin;
+
+  const carWithVin = carsDb.filter((car) => car.vin === vin);
+
+  if (carWithVin.length > 0) {
+    res.status(400).json({ message: `vin ${vin} already exists` });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+};
