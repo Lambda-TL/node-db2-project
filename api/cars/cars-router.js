d2d5d2641b80b2eb21384f9b84538616cd998c92
Jessica <duellje@oregonstate.edu>
2021-02-28 22:35:29 -0500

    removed un-needed file

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const carsModel = require("./cars-model");
 const middleware = require("./cars-middleware");
-const dbConfig = require("../../data/db-config");
 
 router.get("/", (req, res) => {
   carsModel
